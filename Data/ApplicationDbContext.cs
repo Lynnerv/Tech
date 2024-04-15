92207661e30ebac7d0f53f75892203caa64c6b08
lynnerv <aythor_briceno@usmp.pe>
2024-04-15 02:58:07 -0500

    fase 4

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,6 @@ public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
-    {
-    }
+    {}
+        public DbSet<Tech.Models.Contacto> DataContacto {get; set; }
 }
